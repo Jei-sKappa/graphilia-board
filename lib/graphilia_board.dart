6855303c4cdb8746ed99c376b6af2accddf6cb04
Jei-sKappa <guzzojacopo95@gmail.com>
2025-01-16 21:38:40 +0100

    fix: unnecessary notifier export

--- lib/graphilia_board.dart
+++ lib/graphilia_board.dart
@@ -1,6 +1,5 @@
 library graphilia_board;
 
 export 'src/core/core.dart';
-export 'src/presentation/notifier/notifier.dart';
 export 'src/models/models.dart';
 export 'src/presentation/presentation.dart';
