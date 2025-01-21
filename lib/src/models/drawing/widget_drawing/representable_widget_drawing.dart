dc922980082844cf70a0f692d68d85e3d7c57d6e
Jei-sKappa <guzzojacopo95@gmail.com>
2025-01-21 21:29:02 +0100

    fix: RepresentableWidgetDrawing don't override equals and hashcode

--- lib/src/models/drawing/widget_drawing/representable_widget_drawing.dart
+++ lib/src/models/drawing/widget_drawing/representable_widget_drawing.dart
@@ -1,8 +1,9 @@
 import 'dart:ui';
 
+import 'package:equatable/equatable.dart';
 import 'package:graphilia_board/src/models/models.dart';
 
-abstract class RepresentableWidgetDrawing<T> extends WidgetDrawing<T> with RepresentableDrawingMixin {
+abstract class RepresentableWidgetDrawing<T> extends WidgetDrawing<T> with EquatableMixin, RepresentableDrawingMixin {
   const RepresentableWidgetDrawing({
     required super.id,
     required super.zIndex,
@@ -11,6 +12,9 @@ abstract class RepresentableWidgetDrawing<T> extends WidgetDrawing<T> with Repre
 
   @override
   final AnchoredDrawingRepresentation representation;
+
+  @override
+  List<Object?> get props => [...super.props, representation];
 
   @override
   Rect getBounds() => Rect.fromPoints(
