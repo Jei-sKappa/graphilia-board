e37c731c199cb488bb263c7d97a037befdfe9222
Jei-sKappa <guzzojacopo95@gmail.com>
2025-01-31 14:12:28 +0100

    feat(canvas_drawing): define canvas drawing

---
+++ plugins/drawing/graphilia_board_canvas_drawing/lib/src/models/canvas_drawing.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+import 'package:graphilia_board_core/graphilia_board_core.dart';
+
+abstract class CanvasDrawing<T> extends Drawing<T> {
+  const CanvasDrawing({
+    required super.id,
+    required super.zIndex,
+  });
+
+  void draw(
+    BoardState<T> state,
+    Canvas canvas, {
+    required bool isSelected,
+  });
+
+  /// This method is called when the [Drawing] is being created for the first time.
+  ///
+  /// See [DrawInteraction] and [InteractionFeedbackLayer] for more information.
+  void drawFeedback(
+    BoardState<T> state,
+    Canvas canvas,
+  ) =>
+      draw(state, canvas, isSelected: false);
+}
